14cd305a32534b1360b20922262081d6479c1ef7
alkanb <buket.alkan@vanderbilt.edu>
2025-03-20 14:51:14 -0500

    push check

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div className="p-4 text-gray-800 dark:text-gray-200">
       <h1 className="text-2xl font-bold">About Us</h1>
-      <p>This is the about page content.</p>
+      <p>Hey, This is the about page content.</p>
     </div>
   );
 };
